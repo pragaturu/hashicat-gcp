cac9270ca0550da54c647df7cc67b697a2abe0c8
pragaturu <prasreddy@gmail.com>
2021-05-20 17:30:21 +0000

    Added vpc module corrections

--- vpc.tf
+++ vpc.tf
@@ -1,14 +1,14 @@
 module "network" {
-  source  = "terraform-google-modules/network/google"
-  version = "3.2.2"
+  source  = "app.terraform.io/Prasada-training/network/google"
+  version = "2.5.0"
   # insert the 3 required variables here
   network_name = "prasada-network"
   project_id = var.project
-subnets = [
-  {
-    subnet_name   = "gaurav-subnet"
-    subnet_ip     = "10.100.10.0/24"
-    subnet_region = var.region
-  }
-]  
+    subnets = [
+    {
+        subnet_name   = "prasada-subnet"
+        subnet_ip     = "10.100.10.0/24"
+        subnet_region = var.region
+    }
+    ]  
 }
